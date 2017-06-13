7aad6c37dc57a937950ce77a699e10f8657755fc
colon-dee <colondee@protonmail.com>
2017-06-13 19:55:24 -0300

    Adiciona suporte a teclado e arredonda informações

--- main.lua
+++ main.lua
@@ -12,10 +12,10 @@ function love.load()
 	raio = Linha()
 	triangulo = Triangulo()
 	tangent = Linha()
-	
+
 	triangulo.cor = {r = 255, g = 255, b = 255, a = 255}
 	tangent.cor = {r = 255, g = 155, b = 0, a = 255}
-	
+
 	info1 = ''
 	info2 = 	''
 	info3 = ''
@@ -26,21 +26,21 @@ function love.update(dt)
  raio.y = circ.y
  raio.x = circ.x
  raio.largura = circ.raio
- 
+
  tangent.x1 = circ.x + circ.raio
  tangent.y1 = circ.y
- 
+
  tangent.x2 = tangent.x1
  tangent.y2 = tangent.y1 - math.tan(math.rad(angulo))
- 
- info1 = "Sin: "..math.sin(math.rad(angulo))
-	info2 = 	"Cos: "..math.cos(math.rad(angulo))
-	info3 = "Tg: "..math.tan(math.rad(angulo))
-	info4 = "Degree: "..angulo
 
- 
+ info1 = "Sin: "..tonumber(string.format("%.3f", math.sin(math.rad(angulo))))
+	info2 = 	"Cos: "..tonumber(string.format("%.3f", math.cos(math.rad(angulo))))
+	info3 = "Tg: "..tonumber(string.format("%.3f", math.tan(math.rad(angulo))))
+	info4 = "Degree: "..tonumber(string.format("%.3f", angulo))
+
+
  -- Calcula posicão da hipotenusa de acordo com os outroe valores
- 
+
  triangulo.altura = math.sin(math.rad(angulo)) * circ.raio
  triangulo.base = math.cos(math.rad(angulo)) * circ.raio
 end
@@ -50,7 +50,7 @@ function love.draw()
 	raio:draw()
 	triangulo:draw()
 	tangent:draw()
-	
+
 	--debug
 	love.graphics.print(info1, 10, 80, 0, 5)
 	love.graphics.print(info2, 10, 2 * 80, 0, 5)
@@ -58,8 +58,16 @@ function love.draw()
 	love.graphics.print(info4, 10, 4 * 80, 0, 5)
 end
 
-function love.joystickaxis(joystick, axis, value) 
+function love.joystickaxis(joystick, axis, value)
 	if axis == 1 then
 	 angulo = angulo + value
+	end
+end
+
+function love.keypressed(key)
+	if key == "up" then
+		angulo = angulo + 1
+	elseif key == "down" then
+		angulo = angulo - 1
 	end
 end
