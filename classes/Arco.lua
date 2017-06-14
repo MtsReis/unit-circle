a1b614f72e5fcfe03b46f627ff10240c962879ee
colon-dee <colondee@protonmail.com>
2017-06-14 11:47:56 -0300

    Adiciona sine, cos, arc e altera aparência

--- classes/Arco.lua
+++ classes/Arco.lua
@@ -1,15 +1,16 @@
-class.Linha()
+class.Arco()
 
 -- Propriedades padrões
-Linha.x1 = 	love.graphics.getWidth() / 2 / escala
-Linha.y1 = love.graphics.getHeight() / 2 / escala
-Linha.x2 = Linha.x1 + 1
-Linha.y2 = Linha.y1
-Linha.cor = {r = 255, g = 255, b = 255, a = 255}
-Linha.drawMode = "fill"
-Linha.blendMode = "alpha"
+Arco.x = 	love.graphics.getWidth() / 2 / escala
+Arco.y = love.graphics.getHeight() / 2 / escala
+Arco.angulo1 = 0
+Arco.angulo2 = 90
+Arco.raio = 1
+Arco.cor = {r = 155, g = 100, b = 255, a = 255}
+Arco.drawMode = "line"
+Arco.blendMode = "alpha"
 
-function Linha:draw(dt)
+function Arco:draw(dt)
 	-- Armazena cores e BlendMode atuais.
 	local rD, gD, bD, aD = love.graphics.getColor()
 	local blendD = love.graphics.getBlendMode()
@@ -17,7 +18,7 @@ function Linha:draw(dt)
 	-- Aplica as cores e BlendMode do objeto.
 	love.graphics.setColor(self.cor.r, self.cor.g, self.cor.b, self.cor.a)
 	love.graphics.setBlendMode(self.blendMode)
-	love.graphics.line(e(self.x1), e(self.y1), e(self.x2), e(self.y2))
+	love.graphics.arc(self.drawMode, e(self.x), e(self.y), e(self.raio), -math.rad(self.angulo1), -math.rad(self.angulo2))
 
 	-- Retorna a cor e BlendMode aos valores anteriores.
 	love.graphics.setColor(rD, gD, bD, aD)
--- main.lua
+++ main.lua
@@ -1,20 +1,31 @@
 function love.load()
-	angulo = 0;
+	angulo = 0
 	escala = 300
+	
+	background = love.graphics.newImage("back8.png")
+	bgsx = love.graphics.getWidth() / background:getWidth()
+	bgsy = love.graphics.getHeight() / background:getHeight()
 
 	require("util")
+	require("classes/Arco")
 	require("classes/Circunferencia")
 	require("classes/Linha")
 	require("classes/Triangulo")
 
 	-- Propriedades da circunferência
 	circ = Circunferencia()
-	raio = Linha()
+	xaxis = Linha()
+	yaxis = Linha()
 	triangulo = Triangulo()
 	tangent = Linha()
+	sin = Linha()
+	cos = Linha()
+	angArco = Arco()
 
-	triangulo.cor = {r = 255, g = 255, b = 255, a = 255}
 	tangent.cor = {r = 255, g = 155, b = 0, a = 255}
+	sin.cor = {r = 255, g = 155, b = 155, a = 255}
+	cos.cor = {r = 255, g = 0, b = 55, a = 255}
+	triangulo.blendMode = 'add'
 
 	info1 = ''
 	info2 = 	''
@@ -23,33 +34,54 @@ function love.load()
 end
 
 function love.update(dt)
- raio.y = circ.y
- raio.x = circ.x
- raio.largura = circ.raio
+	xaxis.y1 = circ.y
+	xaxis.x1 = 0
+	xaxis.y2 = circ.y
+ xaxis.x2 = love.graphics.getWidth() / escala
+
+	yaxis.y1 = 0
+	yaxis.x1 = circ.x
+	yaxis.y2 = love.graphics.getHeight() / escala
+ yaxis.x2 = circ.x
 
  tangent.x1 = circ.x + circ.raio
  tangent.y1 = circ.y
-
  tangent.x2 = tangent.x1
  tangent.y2 = tangent.y1 - math.tan(math.rad(angulo))
-
+ 
+ sin.x1 = circ.x
+ sin.y1 = circ.y
+ sin.x2 = circ.x
+ sin.y2 = sin.y1 - math.sin(math.rad(angulo))
+ 
+ cos.x1 = circ.x
+ cos.y1 = circ.y
+ cos.x2 = cos.x1 + math.cos(math.rad(angulo))
+ cos.y2 = circ.y
+ 
+ angArco.angulo2 = angulo
+ 
+ -- Calcula posicão da hipotenusa de acordo com os outroe valores
+ triangulo.altura = math.sin(math.rad(angulo)) * circ.raio
+ triangulo.base = math.cos(math.rad(angulo)) * circ.raio
+ 
  info1 = "Sin: "..tonumber(string.format("%.3f", math.sin(math.rad(angulo))))
 	info2 = 	"Cos: "..tonumber(string.format("%.3f", math.cos(math.rad(angulo))))
 	info3 = "Tg: "..tonumber(string.format("%.3f", math.tan(math.rad(angulo))))
 	info4 = "Degree: "..tonumber(string.format("%.3f", angulo))
 
-
- -- Calcula posicão da hipotenusa de acordo com os outroe valores
-
- triangulo.altura = math.sin(math.rad(angulo)) * circ.raio
- triangulo.base = math.cos(math.rad(angulo)) * circ.raio
 end
 
 function love.draw()
+	love.graphics.draw(background, 0, 0, 0, bgsx, bgsy)
 	circ:draw()
-	raio:draw()
+	angArco:draw()
+	xaxis:draw()
+	yaxis:draw()
 	triangulo:draw()
 	tangent:draw()
+	sin:draw()
+	cos:draw()
 
 	--debug
 	love.graphics.print(info1, 10, 80, 0, 5)
@@ -61,6 +93,12 @@ end
 function love.joystickaxis(joystick, axis, value)
 	if axis == 1 then
 	 angulo = angulo + value
+	 
+	 if angulo >= 360 then
+	 	angulo = 0
+	 elseif angulo < 0 then
+	 	angulo = 360
+	 end
 	end
 end
 
@@ -70,4 +108,10 @@ function love.keypressed(key)
 	elseif key == "down" then
 		angulo = angulo - 1
 	end
+	
+	if angulo >= 360 then
+ 	angulo = 0
+ elseif angulo < 0 then
+ 	angulo = 359
+ end
 end
